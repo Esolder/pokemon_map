a7306673c0209d9c73d1b56a72bf4d0fc02dfecd
Esolder <40249820+Esolder@users.noreply.github.com>
2022-11-30 13:09:29 +0300

    Add relation to db models

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from django.db import models  # noqa F401
 
 class Pokemon(models.Model):
@@ -8,5 +10,6 @@ class Pokemon(models.Model):
         return self.title
 
 class PokemonEntity(models.Model):
+    pokemon = models.ForeignKey(Pokemon, verbose_name='Покемон', on_delete=models.CASCADE, null=True, blank=True)
     lat = models.FloatField('Долгота')
     lon = models.FloatField('Широта')
